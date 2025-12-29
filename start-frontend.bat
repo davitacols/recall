17bbee071cbaffd49cea8b994ed764f7246aeb44
David Ansa <davitacols@gmail.com>
2025-12-29 15:36:35 +0100

    Remove bat files and documentation

--- start-frontend.bat
+++
@@ -1,4 +0,0 @@
-@echo off
-cd frontend
-echo Starting React frontend...
-npm start
--- start-redis.bat
+++
@@ -1,8 +0,0 @@
-@echo off
-echo Starting Redis with Docker...
-docker-compose up -d
-echo.
-echo Redis is running on localhost:6379
-echo.
-echo To stop Redis: docker-compose down
-echo To view logs: docker-compose logs -f redis

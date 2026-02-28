4472d157302c08c42939b3e29c3b128d38080dcc
davitacols <davitacols@gmail.com>
2026-02-28 03:14:44 +0100

    seo: expand sitemap with additional public routes

--- api/sitemap.xml.js
+++ api/sitemap.xml.js
@@ -1,16 +1,22 @@
 module.exports = (req, res) => {
+  const urls = [
+    { loc: "https://www.knoledgr.com/", changefreq: "weekly", priority: "1.0" },
+    { loc: "https://www.knoledgr.com/home", changefreq: "weekly", priority: "0.9" },
+    { loc: "https://www.knoledgr.com/login", changefreq: "monthly", priority: "0.8" },
+    { loc: "https://www.knoledgr.com/security", changefreq: "monthly", priority: "0.7" },
+  ];
+
   const xml = `<?xml version="1.0" encoding="UTF-8"?>
 <urlset xmlns="http://www.sitemaps.org/schemas/sitemap/0.9">
-  <url>
-    <loc>https://www.knoledgr.com/</loc>
-    <changefreq>weekly</changefreq>
-    <priority>1.0</priority>
-  </url>
-  <url>
-    <loc>https://www.knoledgr.com/login</loc>
-    <changefreq>monthly</changefreq>
-    <priority>0.8</priority>
-  </url>
+${urls
+  .map(
+    (url) => `  <url>
+    <loc>${url.loc}</loc>
+    <changefreq>${url.changefreq}</changefreq>
+    <priority>${url.priority}</priority>
+  </url>`
+  )
+  .join("\n")}
 </urlset>`;
 
   res.setHeader("Content-Type", "application/xml; charset=utf-8");
